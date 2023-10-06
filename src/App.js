f284b43e0e713bf354ab81403fe4f83e0d2d6133
RashvinSky <domnic.skytouch@gmail.com>
2023-10-06 19:32:49 +0530

    comment header
    comment header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/dashboard';
 
 
@@ -9,22 +9,22 @@ import Login from './auth/login';
 import Header from './layout/header';
 
 function App() {
-  
-  const [header,setHeader]=useState(false)
-    
+
+  const [header, setHeader] = useState(false)
+
   return (
     <div className="App">
-         
-      {header ? <Header/> : null}
-     <Routes>
-       <Route path="/" element={<Login/>}></Route>
-     </Routes>
-     <Routes>
-       <Route path="/dashboard" element={<Dashboard/>}></Route>
-     </Routes>
-     <Routes>
-       <Route path="/header" element={<Header/>}></Route>
-     </Routes>
+      {/* Header Component   */}
+      {header ? <Header /> : null}
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+      </Routes>
+      <Routes>
+        <Route path="/header" element={<Header />}></Route>
+      </Routes>
     </div>
   );
 }
